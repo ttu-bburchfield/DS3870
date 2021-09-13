3a8a8a8b2d8aaa3fa302876ca330dc7ad45a7d54
ttu-bburchfield <89394845+ttu-bburchfield@users.noreply.github.com>
2021-09-13 10:51:53 -0500

    Class 13Sept

--- PigPen/js/app.js
+++ PigPen/js/app.js
@@ -1,9 +1,35 @@
+var arrEmployees;
 $.getJSON("https://www.swollenhippo.com/getEmployeesByAPIKey.php?APIKey=Mickey2021!", function(result){
     console.log(result);
-    $.each(result,function(i,keyField){
-        
+    arrEmployees = result;
+    buildEmployeeCard();
+    $.each(result,function(i,person){
+        console.log(person.FirstName);
+        console.log(person.FirstName + ' ' + person.LastName);
+        $('#txtEmail').val(person.Email);
     })
 })
+
+function buildEmployeeCard(){
+    $.each(arrEmployees,function(i,person){
+        if(person.FirstName != 'John'){
+            let strHTML = '<div class="card col-6 offset-3 mt-5">';
+            strHTML += '<h3 class="text-center"><a href="mailto:' + person.Email + '">' + person.FirstName + ' ' + person.LastName + '</a></h3>';
+            strHTML += '<h4 class="text-center">' + person.Postion +'</h4>';
+            strHTML += '<h4 class="mt-3">Profile Details</h4>';
+            strHTML += '<p>Hourly Rate: ' + person.HourlyRate + '</p>';
+            strHTML += '<p>Address:  586 Southern Lane, Cookeville, TN 38506</p>';
+            strHTML += '<p>Assignment:  Clement Hall</p>';
+            strHTML += '<div class="form-group">';
+            strHTML += '<label for="txtPayRate">Pay Rate</label>';
+            strHTML += '<input id="txtPayRate">';
+            strHTML += '</div>';
+            strHTML += '</div>';
+            $('body').append(strHTML);
+        }
+        
+    });
+}
 
 $('#btnTest').click(function() {
     const decTaxRate = .0925;
